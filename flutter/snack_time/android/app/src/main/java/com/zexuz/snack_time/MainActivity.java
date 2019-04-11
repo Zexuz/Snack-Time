717b5cb7352ed3c6c2bb9f75790a1f5e349efe39
Zexuz <robin.edbom@gmail.com>
2019-04-12 00:16:06 +0200

    Able to get device storage info

--- flutter/snack_time/android/app/src/main/java/com/zexuz/snack_time/MainActivity.java
+++ flutter/snack_time/android/app/src/main/java/com/zexuz/snack_time/MainActivity.java
@@ -1,13 +1,97 @@
 package com.zexuz.snack_time;
 
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.StatFs;
+
+import java.io.File;
+
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
+import io.flutter.plugin.common.MethodChannel.Result;
 
 public class MainActivity extends FlutterActivity {
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
-  }
+    private static final String CHANNEL = "com.zexuz.device/battery";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        GeneratedPluginRegistrant.registerWith(this);
+
+        new MethodChannel(getFlutterView(), CHANNEL).setMethodCallHandler(
+                new MethodCallHandler() {
+                    @Override
+                    public void onMethodCall(MethodCall call, Result result) {
+
+                        switch (call.method){
+                            case "isExternalMemoryAvailable": result.success(isExternalMemoryAvailable());
+                            case "getAvailableInternalMemorySize": result.success(getAvailableInternalMemorySize());
+                            case "getTotalInternalMemorySize": result.success(getTotalInternalMemorySize());
+                            case "getAvailableExternalMemorySize": result.success(getAvailableExternalMemorySize());
+                            case "getTotalExternalMemorySize": result.success(getTotalExternalMemorySize());
+                            default:
+                                result.notImplemented();
+                        }
+                    }
+                });
+    }
+
+    public static boolean isExternalMemoryAvailable() {
+        return !Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED);
+    }
+
+    public static Long getAvailableInternalMemorySize() {
+        File path = Environment.getDataDirectory();
+        StatFs stat = new StatFs(path.getPath());
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN_MR2) {
+            long blockSize = stat.getBlockSizeLong();
+            long availableBlocks = stat.getAvailableBlocksLong();
+            return availableBlocks * blockSize;
+        }
+        return 0L;
+    }
+
+    public static Long getTotalInternalMemorySize() {
+        File path = Environment.getDataDirectory();
+        StatFs stat = new StatFs(path.getPath());
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN_MR2) {
+            long blockSize = stat.getBlockSizeLong();
+            long totalBlocks = stat.getBlockCountLong();
+            return totalBlocks * blockSize;
+        }
+        return 0L;
+    }
+
+    public static Long getAvailableExternalMemorySize() {
+        if (!isExternalMemoryAvailable()) {
+            return 0L;
+        }
+        File path = Environment.getExternalStorageDirectory();
+        StatFs stat = new StatFs(path.getPath());
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN_MR2) {
+            long blockSize = stat.getBlockSizeLong();
+            long availableBlocks = stat.getAvailableBlocksLong();
+            return availableBlocks * blockSize;
+        }
+        return 0L;
+    }
+
+    public static Long getTotalExternalMemorySize() {
+        if (!isExternalMemoryAvailable()) {
+            return 0L;
+        }
+        File path = Environment.getExternalStorageDirectory();
+        StatFs stat = new StatFs(path.getPath());
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN_MR2) {
+            long blockSize = stat.getBlockSizeLong();
+            long totalBlocks = stat.getBlockCountLong();
+            return totalBlocks * blockSize;
+        }
+        return 0L;
+    }
 }
--- flutter/snack_time/lib/main.dart
+++ flutter/snack_time/lib/main.dart
@@ -11,9 +11,8 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class StartRoute extends StatelessWidget{
-
-  StartRoute(){
+class StartRoute extends StatelessWidget {
+  StartRoute() {
     SystemChrome.setEnabledSystemUIOverlays([
       SystemUiOverlay.bottom,
       SystemUiOverlay.top,
@@ -26,15 +25,135 @@ class StartRoute extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('First Route'),
+        appBar: AppBar(
+          title: Text('First Route'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              RaisedButton(
+                  child: Text("Start video"),
+                  onPressed: () => Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => VideoRoute()),
+                      )),
+              RaisedButton(
+                  child: Text("Playground"),
+                  onPressed: () => Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => PlaygroundRoute()),
+                      )),
+            ],
+          ),
+        ));
+  }
+}
+
+class PlaygroundRoute extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _PlaygroundRoute();
+}
+
+class _PlaygroundRoute extends State<PlaygroundRoute> {
+  static const platform = const MethodChannel('com.zexuz.device/battery');
+
+  static const suffix = {
+    0: "Bytes",
+    1: "KB",
+    2: "MB",
+    3: "GB",
+  };
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, crossAxisAlignment: CrossAxisAlignment.center, children: [
+          RaisedButton(
+            onPressed: _isExternalMemoryAvailable,
+            child: Text("Get battery level"),
+          ),
+          Text(_batteryLevel),
+          RaisedButton(
+            onPressed: _getAvailableInternalMemorySize,
+            child: Text("Get internal"),
+          ),
+          Text(_getSizeAsString(_availableInternalMemory)),
+          RaisedButton(
+            onPressed: _getTotalInternalMemorySize,
+            child: Text("Get total internal"),
+          ),
+          Text(_getSizeAsString(_totalInternalMemory)),
+          RaisedButton(
+            onPressed: _getAvailableExternalMemory,
+            child: Text("Get extrnal"),
+          ),
+          Text(_getSizeAsString(_availableExternalMemory)),
+          RaisedButton(
+            onPressed: _getTotalExternalMemorySize,
+            child: Text("Get external total"),
+          ),
+          Text(_getSizeAsString(_totalExternalMemorySize)),
+        ]),
       ),
-      body: RaisedButton(
-          child: Text("Start video"),
-          onPressed: () => Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => VideoRoute()),
-              )),
     );
+  }
+
+  String _batteryLevel = 'Unknown battery level.';
+
+  Future<void> _isExternalMemoryAvailable() async {
+    String batteryLevel;
+    try {
+      batteryLevel = (await platform.invokeMethod('isExternalMemoryAvailable')).toString();
+    } on PlatformException catch (e) {
+      batteryLevel = "Failed to get battery level: '${e.message}'.";
+    }
+
+    setState(() {
+      _batteryLevel = batteryLevel;
+    });
+  }
+
+  double _availableInternalMemory = 0;
+  Future<void> _getAvailableInternalMemorySize() async {
+    int spaceLeft = await platform.invokeMethod('getAvailableInternalMemorySize');
+    setState(() {
+      _availableInternalMemory = spaceLeft.toDouble();
+    });
+  }
+
+  double _totalInternalMemory = 0;
+  Future<void> _getTotalInternalMemorySize() async {
+    int spaceLeft = await platform.invokeMethod('getTotalInternalMemorySize');
+    setState(() {
+      _totalInternalMemory = spaceLeft.toDouble();
+    });
+  }
+
+  double _availableExternalMemory = 0;
+  Future<void> _getAvailableExternalMemory() async {
+    int spaceLeft = await platform.invokeMethod('getAvailableExternalMemorySize');
+    setState(() {
+      _availableExternalMemory = spaceLeft.toDouble();
+    });
+  }
+  double _totalExternalMemorySize= 0;
+  Future<void> _getTotalExternalMemorySize() async {
+    int spaceLeft = await platform.invokeMethod('getTotalExternalMemorySize');
+    setState(() {
+      _totalExternalMemorySize = spaceLeft.toDouble();
+    });
+  }
+
+  String _getSizeAsString(double size) {
+    int i = 0;
+    while (size > 1024) {
+      i++;
+      size /= 1024;
+    }
+
+    return "${suffix[i]} ${size.toStringAsPrecision(4)}";
   }
 }
