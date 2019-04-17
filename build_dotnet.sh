f828e0cdf249c6b432f9123c9378ff68767b8f67
zexuz <robin.edbom@gmail.com>
2019-04-17 18:57:01 +0200

    Updated dotnet build image

--- build_dotnet.sh
+++ build_dotnet.sh
@@ -3,17 +3,12 @@
 docker run --rm -i \
 -v $(pwd):/app \
 -w /app \
-3.0.100-preview3-alpine3.9 \
+mcr.microsoft.com/dotnet/core/sdk:3.0.100-preview3-alpine3.9 \
 dotnet build -c Release -r win-x64 -o ./webapi ./SnackTime.WebApi/SnackTime.WebApi.csproj 
 
 docker run --rm -i \
 -v $(pwd):/app \
 -w /app \
-3.0.100-preview3-alpine3.9 \
+mcr.microsoft.com/dotnet/core/sdk:3.0.100-preview3-alpine3.9 \
 dotnet build -c Release -r win-x64 -o ./media-server ./SnackTime.MediaServer.Runner/SnackTime.MediaServer.Runner.csproj
 
-docker run --rm -i \
--v $(pwd)/app:/app \
--w /app \
-node:8.15.0-jessie \
-/bin/bash -c "yarn install && yarn run build"
--- build_frontend.sh
+++ build_frontend.sh
@@ -1,13 +1,7 @@
 #!/bin/sh
 
 docker run --rm -i \
--v $(pwd):/app \
+-v $(pwd)/app:/app \
 -w /app \
-3.0.100-preview3-alpine3.9 \
-dotnet build -c Release -r win-x64 -o ./webapi ./SnackTime.WebApi/SnackTime.WebApi.csproj 
-
-docker run --rm -i \
--v $(pwd):/app \
--w /app \
-3.0.100-preview3-alpine3.9 \
-dotnet build -c Release -r win-x64 -o ./media-server ./SnackTime.MediaServer.Runner/SnackTime.MediaServer.Runner.csproj
+node:8.15.0-jessie \
+/bin/bash -c "yarn install && yarn run build"
